c81c9676131b43275f0089aa4032b5b45bab7687
Archies98 <desaiarchies1998@gmail.com>
2024-09-16 11:45:31 -0400

    added batch file for scheduling backup to run daily

--- backup.py
+++ backup.py
@@ -2,26 +2,38 @@ import os
 import json
 import time
 
-base = 'D:/Linkedin/'
+def upload_to_s3(file):
+    # complete implementation
+    return True
 
-# Open and read the folders JSON file
-with open('folders.json', 'r') as file:
-    folders = json.load(file)
+def backup():
+    base = 'D:/Linkedin/'
 
-for d in os.walk(base):
-    # print(f'Current Folder: {d[0]}, Subfolders: {d[1]}, Files: {d[2]}')
-    if d[0] not in folders:
-        print(f'Tracking folder {d[0]} now.')
-        folders[d[0]] = {}
+    # Open and read the folders JSON file
+    with open('folders.json', 'r') as file:
+        folders = json.load(file)
 
-    for file in d[2]:
-        if file not in folders[d[0]]:
-            print(f'Tracking file {file} now.')
-            folders[d[0]][file]=os.path.getmtime(d[0]+'/'+file)
+    for d in os.walk(base):
+        # print(f'Current Folder: {d[0]}, Subfolders: {d[1]}, Files: {d[2]}')
+        folder = d[0].replace('\\','/')
+        if d[0] not in folders:
+            print(f'Tracking folder {folder} now.')
+            folders[folder] = {}
 
-            # to-do
-            # upload_to_s3(file)
+        for file in d[2]:
+            if file not in folders[folder]:
+                print(f'Tracking file {folder + file} now.')
+                folders[folder][file]=os.path.getmtime(folder+'/'+file)
+
+                upload_to_s3(file)
+
+            if folders[folder][file] < os.path.getmtime(folder+'/'+file):
+                print(f'Backed up file {file} to latest version.')
 
 
-with open('folders.json','w') as file:
-    file.write(json.dumps(folders))
+    with open('folders.json','w') as file:
+        file.write(json.dumps(folders))
+
+    return True
+
+backup()
