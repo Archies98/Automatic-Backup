46077d366d21a1138a5f2657a907f48c4e442c08
Archies98 <desaiarchies1998@gmail.com>
2024-09-17 11:07:42 -0400

    added the desktop app folder

--- backup.py
+++ backup.py
@@ -1,9 +1,23 @@
 import os
 import json
 import time
+import boto3
+from botocore.config import Config
 
 def upload_to_s3(file):
     # complete implementation
+    with open('configuration.json','r') as config:
+        credentials = config['s3_credentials']
+
+    s3 = boto3.client(
+        service = 's3',
+        region_name = 'us-east-1',
+        aws_access_key_id = credential['AWS_ACCESS_KEY'],
+        aws_secret_access_key = credential['AWS_SECRET_ACCESS_KEY']
+    )
+
+    s3.Bucket('backup-app-storage').upload_file(file, file)
+
     return True
 
 def backup():
@@ -16,16 +30,16 @@ def backup():
     for d in os.walk(base):
         # print(f'Current Folder: {d[0]}, Subfolders: {d[1]}, Files: {d[2]}')
         folder = d[0].replace('\\','/')
-        if d[0] not in folders:
+        if folder not in folders:
             print(f'Tracking folder {folder} now.')
             folders[folder] = {}
 
         for file in d[2]:
             if file not in folders[folder]:
-                print(f'Tracking file {folder + file} now.')
+                print(f'Tracking file {folder + '/' + file} now.')
                 folders[folder][file]=os.path.getmtime(folder+'/'+file)
 
-                upload_to_s3(file)
+                upload_to_s3(folder+'/'+file)
 
             if folders[folder][file] < os.path.getmtime(folder+'/'+file):
                 print(f'Backed up file {file} to latest version.')
