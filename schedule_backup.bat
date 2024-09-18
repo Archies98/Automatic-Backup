f6dd5fdaba1173ae55bc5d6a0ab443b71ff8689a
Archies98 <desaiarchies1998@gmail.com>
2024-09-18 16:18:31 -0400

    added side bar in desktop app

--- schedule_backup.bat
+++ schedule_backup.bat
@@ -1,1 +1,1 @@
-schtasks /create /sc daily /tn "daily backup task" /tr "D:\Projects\Software Engineering\Automatic Backup\run_backup.bat" /st 13:00
+schtasks /create /sc daily /tn "daily backup task" /tr "'D:\Projects\Software Engineering\Automatic Backup\run_backup.bat'" /st 13:00
